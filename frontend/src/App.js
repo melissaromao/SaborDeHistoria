750017ccfc6df3ac6050b58cce91f0e1d73d1146
melissaromao <melissadiascntt@gmail.com>
2025-05-23 18:54:43 -0300

    Incrementando App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import ReceitaForms from "./components/ReceitaForms";
 import ReceitaLista from "./components/ReceitaLista";
+import ReceitaDetalhes from "./components/ReceitaDetalhes"; 
 
 function App() {
   return (
-    <div>
-      <h1>Sabor de História</h1>
-      <ReceitaForms />
-      <ReceitaLista />
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/cadastrar" element={<ReceitaForms />} />
+          <Route path="/receitas" element={<ReceitaLista />} />
+          <Route path="/receita/:id" element={<ReceitaDetalhes />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
