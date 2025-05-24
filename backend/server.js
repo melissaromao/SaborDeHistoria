5a4dca113d48d16edc6a2bf6ac91b4630ba5ff78
melissaromao <melissadiascntt@gmail.com>
2025-05-23 21:03:22 -0300

    Removendo parâmetros sem utilidade na conexão do mongo

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("Banco de dados conectado"))
+mongoose.connect(process.env.MONGO_URL).then(() => console.log("Banco de dados conectado"))
     .catch(err => console.error("Erro ao conectar ao banco:", err));
 
 app.listen(5000, () => console.log("Servidor rodando na porta 5000"));
