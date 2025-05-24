38133924a2b888d1d6aa815a38e6bb79060ad031
melissaromao <melissadiascntt@gmail.com>
2025-05-23 21:05:23 -0300

    Acrescentando rotas ao app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import ReceitaForms from "./components/ReceitaForms";
 import ReceitaLista from "./components/ReceitaLista";
-import ReceitaDetalhes from "./components/ReceitaDetalhes"; 
+import ReceitaDetalhes from "./components/ReceitaDetalhes";
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
       <Navbar />
       <div className="container mt-4">
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/cadastrar" element={<ReceitaForms />} />
+          <Route path="/editar/:id" element={<ReceitaForms />} />
           <Route path="/receitas" element={<ReceitaLista />} />
           <Route path="/receita/:id" element={<ReceitaDetalhes />} />
         </Routes>
